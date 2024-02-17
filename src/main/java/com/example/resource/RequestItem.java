e1734a6b37a44b69bee3a988d71d6e68929958b5
yuugrid <152932000+yuugrid@users.noreply.github.com>
2024-02-17 10:17:24 +0900

    実装完了

---
+++ src/main/java/com/example/resource/RequestItem.java
@@ -0,0 +1,25 @@
+package com.example.resource;
+
+public class RequestItem {
+	
+	private String name;
+	
+	private Integer price;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	public Integer getPrice() {
+		return this.price;
+	}
+
+}
